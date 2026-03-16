8bbe76fb4ad4fb05420c6ea0a7c1a0f07b7af21a
Megha-toran-045 <meghatorantoran@gmail.com>
2026-03-16 20:06:36 +0530

    Create shift_register.v

---
+++ shift_register.v
@@ -0,0 +1,16 @@
+module shift_register(
+input clk,
+input rst,
+input din,
+output reg [3:0] q
+);
+
+always @(posedge clk or posedge rst)
+begin
+if(rst)
+q <= 4'b0000;
+else
+q <= {q[2:0], din};
+end
+
+endmodule
